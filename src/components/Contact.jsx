9cb83fd1d953ba03919973d5bff192c52174106e
Camilo Cárdenas <camicardenasp@gmail.com>
2023-02-06 01:16:56 -0500

    Added WhatsApp API conection through send button in contact.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,24 +24,41 @@ export const Contact = () => {
   }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
+    // e.preventDefault();
+    // setButtonText("Sending...");
+    // let response = await fetch("http://localhost:5000/contact", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   },
+    //   body: JSON.stringify(formDetails),
+    // });
+    // setButtonText("Send");
+    // let result = await response.json();
+    // setFormDetails(formInitialDetails);
+    // if (result.code == 200) {
+    //   setStatus({ succes: true, message: 'Message sent successfully'});
+    // } else {
+    //   setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+    // }
   };
+
+  const mensaje = `
+  Hi my name is ${formDetails.firstName} ${formDetails.lastName}, my phone is ${formDetails.phone}, my email is ${formDetails.email} and I would like to connect with you.
+
+  Also, ${formDetails.message}
+
+  Thank you for your time!
+  `
+
+  const url=`https://api.whatsapp.com/send?phone=573013990324&text=${encodeURIComponent(mensaje)}`;
+
+  const handleClick=(e) => {
+    e.preventDefault();
+    window.open(url);
+    setFormDetails(formInitialDetails);
+  }
+
 
   return (
     <section className="contact" id="connect">
@@ -65,7 +82,7 @@ export const Contact = () => {
                       <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                      <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
@@ -75,7 +92,7 @@ export const Contact = () => {
                     </Col>
                     <Col size={12} className="px-1">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
+                        <button onClick={handleClick}><span>Send</span></button>
                     </Col>
                     {
                       status.message &&
