322b9670ac73f748eccc1cbab2b68a82bfe233e4
Camilo Cárdenas <camicardenasp@gmail.com>
2023-03-12 17:47:07 -0500

    Fixed navbar, footer and onclik Link response.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,23 +8,22 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
+      <img src={logo} alt="Logo" className="footerIcon" />
+      <p>Copyright 2022. All Rights Reserved</p>
+      {/* <Container>
         <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              {/* <a href="https://www.linkedin.com/in/camicardenasp/" target="_blank"><img src={navIcon1} alt="" /></a> */}
-              {/* Por el momento no tengo instagram ni facebook */}
-              {/* <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a> */}
+              <a href="https://www.linkedin.com/in/camicardenasp/" target="_blank"><img src={navIcon1} alt="" /></a>
+              <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
     </footer>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
@@ -6,6 +6,7 @@ import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {BrowserRouter as Router} from "react-router-dom";
+import MenuIcon from '/MenuIcon.svg'
 
 export const NavBar = () => {
 
@@ -26,25 +27,33 @@ export const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
+  const navRef=useRef(null);
+
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   }
 
+  const [toggle, setToggle]=useState(false);
+  const handleLinkClick=() => {
+    setToggle(!toggle);
+  };
+
   return (
     <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Navbar collapseOnSelect expand="lg" className={scrolled? "scrolled":""} ref={navRef}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" className="logoIcon"/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
+            {/* <span className="navbar-toggler-icon"></span> */}
+            <img src={MenuIcon} alt="" className="navbar-toggler-icon" />
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home'); handleLinkClick()}}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink==='skills'? 'active navbar-link':'navbar-link'} onClick={() => { onUpdateActiveLink('skills'); handleLinkClick()}}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink==='projects'? 'active navbar-link':'navbar-link'} onClick={() => { onUpdateActiveLink('projects'); handleLinkClick()}}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
