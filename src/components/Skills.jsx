de0aa837a86f2651f4626e18511ffb53f4696ed7
Camilo Cárdenas <camicardenasp@gmail.com>
2023-03-12 20:12:28 -0500

    Added Skills icons images.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,26 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import vite from "../assets/Logos/vite.svg";
+import figma from "../assets/Logos/figma.svg";
+import python from "../assets/Logos/python.svg";
+import mern from '../assets/Logos/mern.png'
+import mongo from '../assets/Logos/mongodb.svg'
+import express from '../assets/Logos/expressjs-light.svg'
+import express2 from '../assets/Logos/expressjs-dark.svg'
+
+import reactLogo from "../assets/Logos/react.svg";
+import node from '../assets/Logos/node.svg'
+import tailwind from '../assets/Logos/tailwind.svg'
+import bootstrap from '../assets/Logos/bootstrap.svg'
+import HtmlLogo from '../assets/Logos/html-5.svg'
+import CssLogo from '../assets/Logos/css-3.svg'
+import JavaScript from '../assets/Logos/javascript.svg'
+import Git from '../assets/Logos/git.svg'
+import MySql from '../assets/Logos/mysql.svg'
+import PowerBI from '../assets/Logos/powerbi.png'
+
+
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -16,49 +32,105 @@ export const Skills = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 4
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 901 },
+      items: 3
+    },
+    bigmobile: {
+      breakpoint: { max: 800, min: 0 },
       items: 2
     },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
+    smallmobile: {
+      breakpoint: { max: 400, min: 0 },
       items: 1
     }
   };
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>I have knowledge and use the following technologies: React, JavaScript, HTML, CSS, focused on Responsive Web Design and Development. On the other hand, I have knowledge of Python, SQL and Mongo in data handling.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Web Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Data Analysis</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Consulting</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>I have knowledge and use the following technologies: React, JavaScript, HTML, CSS, focused on Responsive Web Design and Development. On the other hand, I have knowledge of Python, SQL and Mongo in data handling.</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <img src={vite} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Web Development</h5>
                 </div>
+                <div className="item">
+                  <img src={figma} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Web Design</h5>
+                </div>
+                <div className="item">
+                  <img src={python} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Data Analysis</h5>
+                </div>
+                <div className="item">
+                  <img src={mern} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>MERN</h5>
+                </div>
+                <div className="item">
+                  <img src={mongo} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>MongoDB</h5>
+                </div>
+                <div className="item">
+                  <img src={express} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Express</h5>
+                </div>
+                <div className="item">
+                  <img src={express2} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Express</h5>
+                </div>
+                <div className="item">
+                  <img src={reactLogo} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>React</h5>
+                </div>
+                <div className="item">
+                  <img src={node} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>NodeJS</h5>
+                </div>
+                <div className="item">
+                  <img src={tailwind} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Tailwind</h5>
+                </div>
+                <div className="item">
+                  <img src={bootstrap} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Bootstrap</h5>
+                </div>
+                <div className="item">
+                  <img src={HtmlLogo} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img src={CssLogo} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={JavaScript} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <img src={Git} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>Git</h5>
+                </div>
+                <div className="item">
+                  <img src={MySql} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>MySQL</h5>
+                </div>
+                <div className="item">
+                  <img src={PowerBI} alt="Image" style={{ maxHeight: "111.5px" }} />
+                  <h5>PowerBI</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
